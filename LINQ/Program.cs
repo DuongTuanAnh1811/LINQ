fab5bdca9452c29b1a8f3645cce9cb346927e1f3
DuongTuanAnh <tuanduong754@gmail.com>
2021-12-08 10:36:30 +0700

    read data from xml file using LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,22 +1,51 @@
 ﻿
+using System.Xml.Linq;
+
 namespace LINQ 
 { 
     class Program
-    { 
+    { public static void QueryXMLFile(string fileName)
+        {
+            var xmlDocument = XDocument.Load(fileName);
+        IEnumerable<string> fileNames = from film in xmlDocument.Descendants("film")
+                                    where (int)film.Element("releaseYear")==2021
+                                    select film.Element("name").Value;
+            foreach (string filmName in fileNames)
+            {
+                Console.WriteLine(filmName);
+            }
+
+        }
         public static void QueryIntNumber()
         {
           int[] numbers = new int[]{ 1, 2, 3,4,5,6,7,8,9,10,11 };
-            var QueryResult = from number in numbers
+            var queryResult = from number in numbers
                               where (number %2)==0 
                               select number;
-            foreach (int number in numbers)
+            foreach (int number in queryResult)
             {
-                Console.WriteLine(number);
+                Console.Write(number+" ");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Convert query result to array ");
+            var arrayOfNumber = queryResult.ToArray();
+            foreach (int number in arrayOfNumber)
+            {
+                Console.Write(number+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Convert query result to list ");
+            var listOfNumber = queryResult.ToList();
+            foreach (int number in listOfNumber)
+            {
+                Console.Write(number+" ");
             }
         }
         static void Main(string[] args)
         {
-           
+            Program.QueryXMLFile("D:/hocthem/LINQ/LINQ/LINQ/films.xml");
+         //  Program.QueryIntNumber();
             Console.ReadKey();
         }
     }
