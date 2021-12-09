23b8b3f596752512ab3629368619df4601db3022
DuongTuanAnh <tuanduong754@gmail.com>
2021-12-09 17:39:32 +0700

    update demo2linq

--- Demo2Linq/Program.cs
+++ Demo2Linq/Program.cs
@@ -3,21 +3,7 @@
     class Program
     {
        
-        class Teacher :Person
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public  double Income { get; set; }
-            public string Address { get; set; }
-            public double Taxcoe { get; set; }
-           TaxCoe tax = new TaxCoe();
-            public double TaxCoe()
-            {
-                return Income*tax.GetTaxCoe(this)/100;
-            }
-        }
-
+     
         
 
         static void Main(string[] args)
---
+++ Demo2Linq/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo2linq
+{
+    class Teacher : Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Income { get; set; }
+        public string Address { get; set; }
+        public double Taxcoe { get; set; }
+        TaxCoe tax = new TaxCoe();
+        public double TaxCoe()
+        {
+            return Income * tax.GetTaxCoe(this) / 100;
+        }
+    }
+
+}
